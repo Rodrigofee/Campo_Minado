955b473b1d4c8fd78f9739955953cc13cd922876
Rodrigofee <rcabral756@gmail.com>
2019-09-19 15:56:01 -0300

    Subindo Projeto

---
+++ src/functions.js
@@ -0,0 +1,39 @@
+const creatBoard = (rows, columns) => {
+    return Array(rows).fill(0).map((_, row) => {
+        return Array(columns).fill(0).map((_, column) => {
+            return {
+                row,
+                column,
+                opened: false,
+                flagger: false,
+                mined: false,
+                exploded: false,
+                nearMines: 0
+            }
+        })
+    })
+}
+
+const spreadMines = (board, minesAmount) => {
+    const rows = board.length
+    const column = board[0].length
+    let minesPlanted = 0
+
+    while (minesPlanted < minesAmount) {
+        const rowSel = parseInt(Math.random() * rows, 10)
+        const columnSel = parseInt(Math.random() * column, 10)
+
+        if (!board[rowSel][columnSel].mined) {
+            board[rowSel][columnSel].mined = true
+            minesPlanted++
+        }
+    }
+}
+
+const createMineBoard = (rows, columns, minesAmount) => {
+    const board = createBoard(rows, column)
+    spreadMines(board, minesAmount)
+    return board
+}
+
+export { createMineBoard }
